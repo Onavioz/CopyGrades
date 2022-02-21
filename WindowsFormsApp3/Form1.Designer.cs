f18587023f598af5071cbdf3acb737978d056b0c
avioz <avioz@DESKTOP-5DEOA4U>
2022-02-21 22:17:49 +0200

    Add project files.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Enabled = false;
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(97, 238);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(271, 94);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "TomaGrade Excel file";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Info;
+            this.button2.Enabled = false;
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(97, 349);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(271, 94);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Target Excel file";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(246, 60);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "1. Write the code of the Course.\r\n2. Write the name of the Table.\r\n3. Choose Toma" +
+    "Grade Excel file.\r\n4. Click and Choose Target Excel file.";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Coral;
+            this.label2.Location = new System.Drawing.Point(24, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Instructions: ";
+            // 
+            // label3
+            // 
+            this.label3.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(140, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(195, 45);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Copy Grades";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(159, 204);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(25, 204);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Name of the Table:";
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.LightSalmon;
+            this.button4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button4.Location = new System.Drawing.Point(266, 201);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(127, 29);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "set Name here";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(25, 163);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(133, 15);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Code of the Course:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(159, 160);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 12;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightSalmon;
+            this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Location = new System.Drawing.Point(266, 156);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 29);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "set Code here";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Location = new System.Drawing.Point(409, 164);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 14;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Enabled = false;
+            this.checkBox2.Location = new System.Drawing.Point(409, 208);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(15, 14);
+            this.checkBox2.TabIndex = 15;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Enabled = false;
+            this.checkBox3.Location = new System.Drawing.Point(378, 279);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(15, 14);
+            this.checkBox3.TabIndex = 16;
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 456);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Copy Grades";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,235 @@
+﻿using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Windows.Forms;
+using System.Linq;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using NPOI.HSSF.UserModel;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Dictionary<string, string> dict = new Dictionary<string, string>();
+        private DataTable dt = new DataTable();
+        private string targetName;
+        private string targetTable;
+        private string targetCodeCourse;
+        private bool AnotherFlag;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (OpenFileDialog dialog = new OpenFileDialog()
+                { Filter = "Excel Files|*.xls;*.xlsx;*.xlsm", ValidateNames = true })
+                {
+                    if (dialog.ShowDialog() == DialogResult.OK)
+                    {
+                        FileStream fileStream = File.Open(dialog.FileName, FileMode.Open, FileAccess.Read);
+                        IExcelDataReader excelDataReader = ExcelReaderFactory.CreateOpenXmlReader(fileStream);
+                        DataSet result = excelDataReader.AsDataSet();
+                        excelDataReader.Close();
+
+                        dt = result.Tables["Worksheet"];
+                        var count = dt.Rows.Count;
+                        var too = dt.Select();
+                        for (int i = 5; i < count; i++)
+                        {
+                            var id = too[i][0].ToString();
+                            var grade = too[i][2].ToString();
+                            dict.Add(id, grade);
+                        }
+                        button2.Enabled = true;
+                        checkBox3.Checked = true;
+                        button1.Enabled = false;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("ERRORs possible:\n1. incorrect file.\n2.file already opened. please close it before.");
+                button2.Enabled = false;
+                AnotherFlag = false;
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (OpenFileDialog dialog = new OpenFileDialog()
+                { Filter = "Excel Files|*.xls;*.xlsx;*.xlsm", ValidateNames = true })
+                {
+                    if (dialog.ShowDialog() == DialogResult.OK)
+                    {
+                        targetName = dialog.FileName;
+                        FileStream fileStream = File.Open(dialog.FileName, FileMode.Open, FileAccess.Read);
+                        IExcelDataReader excelDataReader = ExcelReaderFactory.CreateBinaryReader(fileStream);
+                        DataSet result = excelDataReader.AsDataSet();
+                        excelDataReader.Close();
+                        dt = result.Tables[targetTable];
+                        if (dt is null)
+                        {
+                            MessageBox.Show("ERROR incorrect table name, please try again.");
+                            button2.Enabled = false;
+                            AnotherFlag = true;
+                            textBox1.Clear();
+                            checkBox2.Checked = false;
+                        }
+                        else
+                        {
+                            Cursor.Current = Cursors.WaitCursor;
+                            HSSFWorkbook templateWorkbook;
+                            HSSFSheet sheet;
+                            HSSFRow dataRow;
+
+                            var count = dt.Rows.Count;
+                            var too = dt.Select();
+                            using (var fs = new FileStream(targetName, FileMode.Open, FileAccess.Read))
+                            {
+                                templateWorkbook = new HSSFWorkbook(fs,true);
+                                sheet = (HSSFSheet)templateWorkbook.GetSheet(targetTable);
+
+                                for (int i = 0; i < count; i++)
+                                {
+                                    var id = "0" + too[i][26].ToString();
+                                    dataRow = (HSSFRow)sheet.GetRow(i);
+                                    if (dict.ContainsKey(id))
+                                    {
+                                        var val = dict.Where(pair => pair.Key == id)
+                                        .Select(pair => pair.Value)
+                                        .FirstOrDefault(); 
+                                        dataRow.Cells[19].SetCellValue(val);
+                                    }
+                                    if (i ==12 && !dataRow.Cells[11].ToString().Contains(targetCodeCourse))
+                                    {
+                                        MessageBox.Show("ERROR The code of the course is not matched to the course in the table.\n" +
+                                            "please check and write correct code of the course or correct name of table");
+                                        targetTable = "";
+                                        targetCodeCourse = "";
+                                        textBox1.Clear();
+                                        textBox2.Clear();
+                                        button2.Enabled = false;
+                                        checkBox1.Checked = false;
+                                        checkBox2.Checked = false;
+                                        AnotherFlag = true;
+                                        return;
+                                    }  
+                                }
+                            }
+                            using (var fs = new FileStream(targetName, FileMode.Open, FileAccess.Write))
+                            {
+                                templateWorkbook.Write(fs);
+                            }
+                            Cursor.Current = Cursors.Default;
+                            MessageBox.Show("Copy grades successfully!");
+                            button2.Enabled = false;
+                            targetTable = "";
+                            targetCodeCourse = "";
+                            checkBox1.Checked = false;
+                            checkBox2.Checked = false;
+                            AnotherFlag = true;
+                            textBox1.Clear();
+                            textBox2.Clear();
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("ERROR please close the excel file you're tring to work with");
+            }
+        }
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Equals(""))
+                MessageBox.Show("ERROR please write the table name.");
+            else if (textBox1.Text.Length == 1 && !AnotherFlag)
+            {
+                targetTable = textBox1.Text[0].ToString().ToUpper();
+                button1.Enabled = true;
+                checkBox2.Checked = true;
+            }
+            else if(!AnotherFlag)
+            {
+                targetTable = textBox1.Text[0].ToString().ToUpper() + textBox1.Text.Substring(1);
+                button1.Enabled = true;
+                checkBox2.Checked = true;
+            }
+            else if (AnotherFlag)
+            {
+                targetTable = textBox1.Text[0].ToString().ToUpper() + textBox1.Text.Substring(1);
+                button2.Enabled = true;
+                checkBox2.Checked = true;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            if (textBox2.Text.Equals(""))
+                MessageBox.Show("ERROR please write the code of the course.");
+            else
+            {
+                try
+                {
+                    targetCodeCourse =int.Parse(textBox2.Text).ToString();
+                    checkBox1.Checked = true;
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("ERROR please write numbers only.");
+                }
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
